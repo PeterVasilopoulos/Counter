3e80e04a22aef1c796f0ab03309d17495bd09e52
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2023-04-07 15:06:02 -0400

    Added basic html and css

--- server.py
+++ server.py
@@ -1,3 +1,9 @@
 from flask import Flask, render_template, session
 app = Flask(__name__)
 
+@app.route('/')
+def counter():
+    return render_template("counter.html")
+
+if __name__ == "__main__":
+    app.run(debug = True)
